d605bc1d45d2c17b830fb87b576e5d0cb341d8e0
Hasan Ali <hasanhaja@gmail.com>
2023-04-19 22:48:51 +0100

    Struggling to get this to work correctly

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,24 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var nums = [_]u8{ 3, 2, 4, 1 };
+    var nums = [_]u8{ 10, 3, 2, 1, 9 };
     std.debug.print("Nums: {any}\n", .{nums});
-    quicksort(&nums, 0, 3);
+    quicksort(&nums, 0, nums.len - 1);
     std.debug.print("Nums: {any}\n", .{nums});
 }
 
 fn quicksort(nums: []u8, low: usize, high: usize) void {
-    std.debug.print("In quicksort, low = {d}, high = {d}\n", .{ low, high });
     if (low < high) {
         var p = parition(nums, low, high);
-
-        quicksort(nums, low, p - 1);
-        quicksort(nums, p + 1, high);
+        std.debug.print("Partition: {d}\n", .{p});
+        if (p == 0) {
+            quicksort(nums, p + 1, high);
+        } else if (p == (nums.len - 1)) {
+            quicksort(nums, low, p - 1);
+        } else {
+            quicksort(nums, low, p - 1);
+            quicksort(nums, p + 1, high);
+        }
     }
 }
 
@@ -21,17 +26,31 @@ fn parition(nums: []u8, low: usize, high: usize) usize {
     // Picking the last element of the slice
     const pivot = nums[high];
 
-    var i = low;
+    // Index of element less than and closest to pivot
+    var i: usize = undefined;
 
-    for (nums[low..high]) |*n| {
+    for (nums[low..high]) |*n, j| {
         if (n.* <= pivot) {
+            // it needs to be set to 0 on first match
+            // after first match, i  == low
+            if (i > high) {
+                i = low;
+                continue;
+            }
+
             i += 1;
+
+            std.mem.swap(u8, &nums[i], &nums[j]);
         }
     }
 
-    std.mem.swap(u8, &nums[i], &nums[high]);
-
-    return i;
+    if (i > high) {
+        std.mem.swap(u8, &nums[low], &nums[high]);
+        return low;
+    } else {
+        std.mem.swap(u8, &nums[i + 1], &nums[high]);
+        return i + 1;
+    }
 }
 
 // This is the same as std.mem.swap
