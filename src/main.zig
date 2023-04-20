fe619c3d616433e64ea454536a0d7740af00e631
Hasan Ali <hasanhaja@gmail.com>
2023-04-20 09:14:52 +0100

    Very close to debugging the issue

--- src/main.zig
+++ src/main.zig
@@ -27,30 +27,28 @@ fn parition(nums: []u8, low: usize, high: usize) usize {
     const pivot = nums[high];
 
     // Index of element less than and closest to pivot
-    var i: usize = undefined;
+    var i: ?usize = null;
 
     for (nums[low..high]) |*n, j| {
+        std.debug.print("intermediate slices: {any}\n", .{nums[low .. high + 1]});
         if (n.* <= pivot) {
             // it needs to be set to 0 on first match
             // after first match, i  == low
-            if (i > high) {
+            if (i == null) {
                 i = low;
+                std.mem.swap(u8, &nums[i.?], &nums[j]);
                 continue;
             }
 
-            i += 1;
-
-            std.mem.swap(u8, &nums[i], &nums[j]);
+            i = i.? + 1;
+            std.mem.swap(u8, &nums[i.?], &nums[j]);
         }
     }
 
-    if (i > high) {
-        std.mem.swap(u8, &nums[low], &nums[high]);
-        return low;
-    } else {
-        std.mem.swap(u8, &nums[i + 1], &nums[high]);
-        return i + 1;
-    }
+    const parition_index = if (i) |index| index + 1 else low;
+    std.mem.swap(u8, &nums[parition_index], &nums[high]);
+    std.debug.print("Partition swapped slice: {any}\n", .{nums[low .. high + 1]});
+    return parition_index;
 }
 
 // This is the same as std.mem.swap
