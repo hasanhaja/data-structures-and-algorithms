0cd0fb7135ef59c37d583a6ebee71b4b415c139d
Hasan Ali <hasanhaja@gmail.com>
2023-05-16 22:47:45 +0100

    Adjust swap index to be within slice range

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var nums = [_]u8{ 10, 3, 2, 1, 9 };
+    var nums = [_]u8{ 11, 10, 3, 2, 1, 9, 12 };
+    // var nums = [_]u8{ 10, 3, 2, 1, 9 };
     std.debug.print("Nums: {any}\n", .{nums});
     quicksort(&nums, 0, nums.len - 1);
     std.debug.print("Nums: {any}\n", .{nums});
@@ -30,24 +31,23 @@ fn parition(nums: []u8, low: usize, high: usize) usize {
     var i: ?usize = null;
 
     for (nums[low..high]) |*n, j| {
-        std.debug.print("intermediate slices: {any}\n", .{nums[low .. high + 1]});
         if (n.* <= pivot) {
             // it needs to be set to 0 on first match
             // after first match, i  == low
             if (i == null) {
                 i = low;
-                std.mem.swap(u8, &nums[i.?], &nums[j]);
+                // when swapping i and j, they both need to be with the slice ranges
+                std.mem.swap(u8, &nums[i.?], &nums[j + low]);
                 continue;
             }
 
             i = i.? + 1;
-            std.mem.swap(u8, &nums[i.?], &nums[j]);
+            std.mem.swap(u8, &nums[i.?], &nums[j + low]);
         }
     }
 
     const parition_index = if (i) |index| index + 1 else low;
     std.mem.swap(u8, &nums[parition_index], &nums[high]);
-    std.debug.print("Partition swapped slice: {any}\n", .{nums[low .. high + 1]});
     return parition_index;
 }
 
