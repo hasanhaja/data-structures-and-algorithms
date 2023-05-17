38361fddbbbe5529be57525f9a33902d604f3a0f
Hasan Ali <hasanhaja@gmail.com>
2023-05-17 22:21:13 +0100

    Refactor expect to expectEqualSlices

--- src/quicksort/quicksort.zig
+++ src/quicksort/quicksort.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const expect = std.testing.expect;
+const expectEqualSlices = std.testing.expectEqualSlices;
 
 pub fn quicksort(nums: []u8, low: usize, high: usize) void {
     if (low < high) {
@@ -38,54 +38,54 @@ test "sorts [1]" {
     var nums = [_]u8{1};
     var sorted = [_]u8{1};
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [2, 2, 2, 1, 1, 8, 3, 3, 3]" {
     var nums = [_]u8{ 2, 2, 2, 1, 1, 8, 3, 3, 3 };
     var sorted = [_]u8{ 1, 1, 2, 2, 2, 3, 3, 3, 8 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [2, 0, 2, 1, 1, 8, 3, 0, 3]" {
     var nums = [_]u8{ 2, 0, 2, 1, 1, 8, 3, 0, 3 };
     var sorted = [_]u8{ 0, 0, 1, 1, 2, 2, 3, 3, 8 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [1, 2, 3]" {
     var nums = [_]u8{ 1, 2, 3 };
     var sorted = [_]u8{ 1, 2, 3 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [2, 3, 1]" {
     var nums = [_]u8{ 2, 3, 1 };
     var sorted = [_]u8{ 1, 2, 3 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [3, 9, 6, 14, 29, 5]" {
     var nums = [_]u8{ 3, 9, 6, 14, 29, 5 };
     var sorted = [_]u8{ 3, 5, 6, 9, 14, 29 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [10, 3, 2, 1, 9]" {
     var nums = [_]u8{ 10, 3, 2, 1, 9 };
     var sorted = [_]u8{ 1, 2, 3, 9, 10 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
 
 test "sorts [11, 10, 3, 2, 1, 9, 12]" {
     var nums = [_]u8{ 11, 10, 3, 2, 1, 9, 12 };
     var sorted = [_]u8{ 1, 2, 3, 9, 10, 11, 12 };
     quicksort(&nums, 0, nums.len - 1);
-    try expect(std.mem.eql(u8, &nums, &sorted));
+    try expectEqualSlices(u8, &nums, &sorted);
 }
